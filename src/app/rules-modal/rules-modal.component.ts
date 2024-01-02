0219043a167fc5805266cd3c41b699e952163828
Juan Lagunas <dev.jlagunas@gmail.com>
2024-01-02 12:31:46 +0900

    Rules modal component and score component styling and logic completed

--- src/app/rules-modal/rules-modal.component.ts
+++ src/app/rules-modal/rules-modal.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-rules-modal',
   standalone: true,
   imports: [],
   templateUrl: './rules-modal.component.html',
-  styleUrl: './rules-modal.component.scss'
+  styleUrl: './rules-modal.component.scss',
 })
 export class RulesModalComponent {
+  @ViewChild('myDialog') dialog!: ElementRef;
 
+  openDialog(): void {
+    this.dialog.nativeElement.showModal();
+  }
+
+  closeDialog(): void {
+    this.dialog.nativeElement.close();
+  }
 }
