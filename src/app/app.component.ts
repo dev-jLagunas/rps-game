cef2935dfd7d1ddb9ddee58294dfaf55a87b344e
Juan Lagunas <dev.jlagunas@gmail.com>
2024-01-02 09:44:34 +0900

    Project components, routes, and initial foundational layout created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ScoreComponent } from './score/score.component';
+import { GameplayComponent } from './gameplay/gameplay.component';
+import { RulesModalComponent } from './rules-modal/rules-modal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ScoreComponent,
+    GameplayComponent,
+    RulesModalComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'rps-game';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { GameplayComponent } from './gameplay/gameplay.component';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeScreenComponent },
+  { path: 'game', component: GameplayComponent },
+];
